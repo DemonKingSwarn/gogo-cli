7b453a15ca60d5bdcfb27dbc7bacdfef0585650e
DemonKingSwarn <rockingswarn@gmail.com>
2023-07-24 19:30:38 +0530

    chore: minor change to chromecast-ing function

--- gogo_cli/core/utils/__cast__.py
+++ gogo_cli/core/utils/__cast__.py
@@ -1,4 +1,10 @@
 import os
 
 def open_cast(url):
-    os.system(f"catt cast \"{url}\"")
+    try:
+        os.system(f"catt cast \"{url}\"")
+    except Exception as e:
+        os.system("catt scan")
+        ip = input("Enter the ip of your chromecast: ")
+        os.system(f"catt -d \"{ip}\" cast \"{url}\"")
+
